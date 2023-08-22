b301e2c208b6ffb81f57b20634401e9ef886078f
Aniss20 <anis.siouda@dm01-it-tbml02.anl.dmgt.net>
2023-08-22 14:18:38 +0100

    Grocery List

--- src/App.js
+++ src/App.js
@@ -1,42 +1,96 @@
-import React, {useState} from "react";
-import "./App.css"
-import {v4 as uuidv4} from "uuid";
-import "./Item.js"
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+import { v4 as uuidv4 } from "uuid";
+
+const arr = () => {
+  let data = localStorage.getItem("data");
+  if (data) return JSON.parse(localStorage.getItem("data"));
+  else return [];
+};
 
 function App() {
   const [item, setItem] = useState("");
-  const [list, setList] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [editId, setEditId] = useState();
+  const [list, setList] = useState(arr);
+  const [error, setError] = useState("");
+
   const handleSubmit = (e) => {
     const newItem = {
       id: uuidv4(),
-      item,
+      item: item,
       complete: false,
     };
     e.preventDefault();
-    if (item) {
+    if (item && item.length <= 25 && !edit) {
       setList([...list, newItem]);
       setItem("");
-    }
+      setError("");
+    } else if (item && item.length <= 25 && edit && editId) {
+      setList(
+        list.map((el) => {
+          if (el.id === editId) {
+            return { ...el, item: item };
+          }
+          return el;
+        })
+      );
+      setItem("");
+      setEditId(null);
+      setEdit(false);
+      setError("");
+    } else if (!item) setError("Item cannot be blank.");
+    else if (item.length > 25) setError("Character limit is 25.");
   };
 
-  const handleChange = e => {
-    setItem (e.target.value);
+  React.useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(list));
+  }, [list]);
 
+  const handleChange = (e) => {
+    setItem(e.target.value);
   };
 
-  return(
-    <div className= "App">
-      <h1>Shopping list</h1>
-      <form onSubmit = {handleSubmit}>
-        <input type = "text" value={item} onChange={handleChange} />
-        <button type = "submit">ADD</button>
+  return (
+    <div className="App">
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={item}
+          placeholder="Enter the items"
+          onChange={handleChange}
+        />
+        {edit ? (
+          <button className="btn" type="submit">
+            Edit Item
+          </button>
+        ) : (
+          <button className="btn" type="submit">
+            Add Item
+          </button>
+        )}
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
+      <div>
+        {list.map((c, id) => (
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            list={list}
+            setList={setList}
+            complete={c.complete}
+            setItem={setItem}
+            setEdit={setEdit}
+            setEditId={setEditId}
+          />
+        ))}
+      </div>
     </div>
   );
-
-  
 }
 
 export default App;
-
-
