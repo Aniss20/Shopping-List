eb08cc2a321eb4a89568d9b48a9ff0b3885df38a
Aniss20 <anis.siouda@dm01-it-tbml02.anl.dmgt.net>
2023-08-22 14:22:03 +0100

    Grocery List

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import "./App.css";
-import Item from "./components/Item";
 import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+import Item from "./Item";
 
 const arr = () => {
   let data = localStorage.getItem("data");
